5d47009186d3cab63f70f76c5f25828f0e94372f
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-07-07 20:49:54 +0100

    Update: move functions from main to engine

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import tcod
 # Modules folder packages
-from modules.actions import EscapeAction, MovementAction
+from modules.engine import Engine
 from modules.input_handlers import EventHandler    
+from modules.entity import Entity
 
 # None because it returns no values
 def main() -> None:
+    # Screen size variables
+	screen_width = 80
+	screen_height = 50
+
     # Declare tileset
 	tileset = tcod.tileset.load_tilesheet(
 		"data/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -14,14 +19,14 @@ def main() -> None:
 	# declare function to handle events
 	event_handler = EventHandler()	
 
-	# Screen size variables
-	screen_width = 80
-	screen_height = 50
-	
-	# Player position variables
-	player_x = int(screen_width / 2)
-	player_y = int(screen_height / 2)
-	
+	# declare entities
+	player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255,255,255))
+	npc = Entity(int(screen_width / 2 + 5), int(screen_height / 2), "T", (255,255,255))
+	entities = {npc, player}
+
+	# Instance the engine class to render entities and handle inputs
+	engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
 	""" Create console to display """
 	with tcod.context.new_terminal(
 		screen_width,
@@ -34,26 +39,9 @@ def main() -> None:
 		root_console = tcod.Console(screen_width, screen_height, order="F")
 
 		while True:
-			root_console.print(x=player_x, y=player_y, string="@")
-			# Displays the console in root_console
-			context.present(root_console)
-
-			root_console.clear()
-			# Process the events in the game
-			for event in tcod.event.wait():
-       
-				action = event_handler.dispatch(event)
-    
-				if action is None:
-					continue
-
-				if isinstance(action, MovementAction):
-					player_x += action.dx
-					player_y += action.dy
-
-				elif isinstance(action, EscapeAction):
-					raise SystemExit()
-
+			engine.render(console=root_console, context=context)
+			events = tcod.event.wait()
+			engine.handle_events(events)
 
 
 if __name__ == "__main__":
